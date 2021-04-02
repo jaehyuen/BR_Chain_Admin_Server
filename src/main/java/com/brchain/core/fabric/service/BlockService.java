24ba4838ef6981c7c4b00b8f79ef9afac9469c94
jaehyuen <violet6520@naver.com>
2021-04-02 12:49:41 +0900

    [2021/04/02 이재현] 저장할 블록데이터 테스트 진행중...

--- src/main/java/com/brchain/core/fabric/service/BlockService.java
+++ src/main/java/com/brchain/core/fabric/service/BlockService.java
@@ -2,8 +2,19 @@ package com.brchain.core.fabric.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.codec.binary.Hex;
+import org.hyperledger.fabric.protos.common.Common;
+import org.hyperledger.fabric.protos.common.Common.BlockData;
+import org.hyperledger.fabric.protos.common.Common.ChannelHeader;
+import org.hyperledger.fabric.protos.common.Common.Header;
+import org.hyperledger.fabric.protos.common.Common.SignatureHeader;
+import org.hyperledger.fabric.protos.msp.Identities.SerializedIdentity;
+import org.hyperledger.fabric.protos.peer.ProposalResponsePackage.ProposalResponse;
+import org.hyperledger.fabric.protos.peer.TransactionPackage.ChaincodeActionPayload;
+import org.hyperledger.fabric.protos.peer.TransactionPackage.Transaction;
+import org.hyperledger.fabric.protos.peer.TransactionPackage.TransactionAction;
 import org.hyperledger.fabric.sdk.BlockInfo;
 import org.hyperledger.fabric.sdk.BlockInfo.EnvelopeInfo;
 import org.springframework.stereotype.Service;
@@ -12,9 +23,10 @@ import com.brchain.common.dto.ResultDto;
 import com.brchain.core.channel.dto.ChannelInfoDto;
 import com.brchain.core.fabric.dto.BlockAndTxDto;
 import com.brchain.core.fabric.dto.BlockDto;
-import com.brchain.core.fabric.entity.BlockEntity;
 import com.brchain.core.fabric.repository.BlockRepository;
 import com.brchain.core.util.Util;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import lombok.RequiredArgsConstructor;
@@ -100,7 +112,35 @@ public class BlockService {
 
 			saveBLock(blockDto);
 		}
-
+		
+		// 디비에 저장할 데이터 테스트중...
+//		block.getTransactionCount();
+//		block.getTransActionsMetaData();
+//		block.getBlock().
+//		
+//		BlockData test1 =block.getBlock().getData();
+//		System.out.println(test1.getDataCount());
+//		BlockData test2=  Common.BlockData.parseFrom(test1.getData(0));
+//		Transaction test3 = Transaction.parseFrom(test1.getData(0));
+//		
+//		TransactionAction test4=TransactionAction.parseFrom(test3.toByteString());
+//		Header test5= Header.parseFrom(test4.getHeader());
+//		ChannelHeader test6= ChannelHeader.parseFrom(test5.getChannelHeader());
+//		
+////		ChannelHeader test11= ChannelHeader.parseFrom(test6.get);
+//		Map<FieldDescriptor, Object> test10 = test6.getAllFields();
+////		test6.get
+//		SignatureHeader test7=SignatureHeader.parseFrom(test5.getSignatureHeader());
+//		
+//		SerializedIdentity test8=  SerializedIdentity.parseFrom(test7.getCreator());
+//		test8.getMspid();
+//		test8.get
+		
+//		ByteString test5 = test4.getPayload();
+//		ProposalResponse test6=ProposalResponse.parseFrom(test4.toByteString());
+//		ChaincodeActionPayload test5= ChaincodeActionPayload.parseFrom(test4.toByteString());
+//		test1.get
+//		block.getTransActionsMetaData()
 		// 블록 내 트렌젝션 순회
 		for (EnvelopeInfo envelopeInfo : block.getEnvelopeInfos()) {
 			transactionService.inspectTransaction(envelopeInfo, channelInfoDto, blockDto);

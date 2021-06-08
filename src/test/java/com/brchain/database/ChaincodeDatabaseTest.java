130d11eb70f7473a0801903b436238a2cb86d421
jaehyuen <violet6520@naver.com>
2021-06-08 20:22:09 +0900

    [2021/06/08 이재현] 체인코드(채널) 조회 테스트 추가 외1
    1. 체인코드(채널) 조회 테스트 추가
    2. 셋업에 체인코드(채널) 등록 추가

--- src/test/java/com/brchain/database/ChaincodeDatabaseTest.java
+++ src/test/java/com/brchain/database/ChaincodeDatabaseTest.java
@@ -3,18 +3,15 @@ package com.brchain.database;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.brchain.core.chaincode.dto.CcSummaryDto;
+import com.brchain.core.chaincode.entitiy.CcInfoChannelEntity;
 import com.brchain.core.chaincode.entitiy.CcInfoEntity;
 import com.brchain.core.chaincode.entitiy.CcInfoPeerEntity;
 import com.brchain.core.chaincode.repository.CcInfoChannelRepository;
@@ -108,7 +105,7 @@ public class ChaincodeDatabaseTest {
 		ChannelInfoEntity channelInfoEntity3 = createChannelInfoEntity("haha-channel");
 		channelInfoEntity3 = channelInfoRepository.save(channelInfoEntity3);
 
-		//채널 피어 등록
+		// 채널 피어 등록
 		ChannelInfoPeerEntity channelInfoPeerEntity = createChannelInfoPeerEntity(channelInfoEntity1, conInfoEntity1);
 		channelInfoPeerEntity = channelInfoPeerRepository.save(channelInfoPeerEntity);
 		channelInfoPeerEntity = createChannelInfoPeerEntity(channelInfoEntity1, conInfoEntity2);
@@ -131,6 +128,25 @@ public class ChaincodeDatabaseTest {
 		channelInfoPeerEntity = channelInfoPeerRepository.save(channelInfoPeerEntity);
 		channelInfoPeerEntity = createChannelInfoPeerEntity(channelInfoEntity3, conInfoEntity5);
 		channelInfoPeerEntity = channelInfoPeerRepository.save(channelInfoPeerEntity);
+
+		// 체인코드 채널 등록
+		CcInfoChannelEntity ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity1, ccInfoEntity1);
+		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
+		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity1, ccInfoEntity2);
+		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
+		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity1, ccInfoEntity3);
+		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
+		
+		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity2, ccInfoEntity1);
+		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
+		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity2, ccInfoEntity2);
+		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
+
+		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity2, ccInfoEntity1);
+		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
+		
+		
+
 	}
 
 	@Test
@@ -151,17 +167,15 @@ public class ChaincodeDatabaseTest {
 		System.out.println("************************ 체인코드_피어_정보_조회_테스트1 종료 ************************");
 
 	}
-	
+
+	// 아직 준비중
 	@Test
 	public void 체인코드_피어_정보_조회_테스트2() throws Exception {
 
 		System.out.println("************************ 체인코드_피어_정보_조회_테스트2 시작 ************************");
 
 		// given
-//		CcInfoEntity ccInfoEntity = ccInfoRepository.findAll().get(0);
-//		
-//		ConInfoEntity conInfoEntity = conInfoRepository.findByConPort("1111").get();
-//		
+
 		// when
 		List<CcInfoPeerEntity> result = ccInfoPeerRepository.findByCcId(1l);
 
@@ -190,7 +204,7 @@ public class ChaincodeDatabaseTest {
 		System.out.println("************************ 활성화_가능한_체인코드_조회_테스트 종료 ************************");
 
 	}
-	
+
 	@Test
 	public void 체인코드_요약_조회_테스트() throws Exception {
 
@@ -208,59 +222,34 @@ public class ChaincodeDatabaseTest {
 		System.out.println("************************ 체인코드_요약_조회_테스트 종료 ************************");
 
 	}
-//
-//	@Test
-//	public void 체인코드_피어_조회_테스트() throws Exception {
-//
-//		System.out.println("************************ 체인코드_피어_정보_저장_조회_테스트 시작 ************************");
-//
-//		// given
-//		CcInfoEntity ccInfoEntity = createCcInfoEntity("test-chaincode");
-//		ccInfoEntity = ccInfoRepository.save(ccInfoEntity);
-//
-//		ConInfoEntity conInfoEntity1 = createConInfoEntity("peer0.orgtest.com", "1111");
-//		conInfoEntity1 = conInfoRepository.save(conInfoEntity1);
-//
-//		ConInfoEntity conInfoEntity2 = createConInfoEntity("peer1.orgtest.com", "1112");
-//		conInfoEntity2 = conInfoRepository.save(conInfoEntity2);
-//
-//		ChannelInfoEntity channelInfoEntity = createChannelInfoEntity("test-channel");
-//		channelInfoEntity = channelInfoRepository.save(channelInfoEntity);
-//
-//		CcInfoPeerEntity ccInfoPeerEntity1 = createCcInfoPeerEntity(ccInfoEntity, conInfoEntity1);
-//		CcInfoPeerEntity ccInfoPeerEntity2 = createCcInfoPeerEntity(ccInfoEntity, conInfoEntity2);
-//
-//		// when
-//		ccInfoPeerEntity1 = ccInfoPeerRepository.save(ccInfoPeerEntity1);
-//		ccInfoPeerEntity2 = ccInfoPeerRepository.save(ccInfoPeerEntity2);
-//
-//		System.out.println("ccInfoPeerEntity1 : " + ccInfoPeerEntity1);
-//		System.out.println("ccInfoPeerEntity2 : " + ccInfoPeerEntity2);
-//
-//		List<CcInfoPeerEntity> resultList = ccInfoPeerRepository.findByCcId(ccInfoEntity.getId());
-//
-////		// then
-//		assertThat(ccInfoEntity.getCcName()).isEqualTo("test-chaincode");
-//		assertThat(ccInfoEntity.getCcPath()).isEqualTo("/src/test/chaincode/test.go");
-//		assertThat(ccInfoEntity.getCcLang()).isEqualTo("golang");
-//		assertThat(ccInfoEntity.getCcDesc()).isEqualTo("this is test chaincode");
-//		assertThat(ccInfoEntity.getCcVersion()).isEqualTo("1");
-//
-//		System.out.println("************************ 체인코드_피어_정보_저장_조회_테스트 종료 ************************");
-//
-//	}
+	
+	@Test
+	public void 체인코드_채널_조회_테스트1() throws Exception {
+
+		System.out.println("************************ 체인코드_채널_조회_테스트1 시작 ************************");
+
+		// given
+
+		// when
+		List<CcInfoChannelEntity> result = ccInfoChannelRepository.findByChannelName("test-channel");
+
+		// then
+		System.out.println(result);
+		assertThat(result.size()).isEqualTo(3);
+
+		System.out.println("************************ 체인코드_채널_조회_테스트1 종료 ************************");
+
+	}
 
 	private CcInfoEntity createCcInfoEntity(String param) {
 
 		CcInfoEntity ccInfoEntity = new CcInfoEntity();
 
-		// ccInfoEntity.setId(1L);
 		ccInfoEntity.setCcName(param);
 		ccInfoEntity.setCcPath("/src/test/chaincode/test.go");
 		ccInfoEntity.setCcLang("golang");
 		ccInfoEntity.setCcDesc("this is test chaincode");
 		ccInfoEntity.setCcVersion("1");
-		// ccInfoEntity.setCreatedAt(LocalDateTime.now());
 
 		return ccInfoEntity;
 
@@ -331,6 +320,19 @@ public class ChaincodeDatabaseTest {
 		// channelInfoPeerEntity.set
 
 		return channelInfoPeerEntity;
+
+	}
+
+	private CcInfoChannelEntity createCcInfoChannelEntity(ChannelInfoEntity channelInfoEntity,
+			CcInfoEntity ccInfoEntity) {
+
+		CcInfoChannelEntity ccInfoChannelEntity = new CcInfoChannelEntity();
+
+		ccInfoChannelEntity.setCcInfoEntity(ccInfoEntity);
+		ccInfoChannelEntity.setChannelInfoEntity(channelInfoEntity);
+		ccInfoChannelEntity.setCcVersion("1");
+
+		return ccInfoChannelEntity;
 
 	}
 

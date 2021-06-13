c65530c9451ae13f2683f9c63ff7ab6717810e9d
jaehyuen <violet6520@naver.com>
2021-06-13 20:23:17 +0900

    [2021/06/13 이재현] 채널 관련 디비 테스트 추가
    1. 채널 요약 조회 테스트 추가
     - 트랜잭션, 블록 테스트 데이터 추가해야됨..

--- src/test/java/com/brchain/database/ChannalDatabaseTest.java
+++ src/test/java/com/brchain/database/ChannalDatabaseTest.java
@@ -1,8 +1,7 @@
 package com.brchain.database;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
+import java.util.Random;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,13 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import com.brchain.core.chaincode.dto.CcSummaryDto;
 import com.brchain.core.chaincode.entitiy.CcInfoChannelEntity;
 import com.brchain.core.chaincode.entitiy.CcInfoEntity;
 import com.brchain.core.chaincode.entitiy.CcInfoPeerEntity;
 import com.brchain.core.chaincode.repository.CcInfoChannelRepository;
 import com.brchain.core.chaincode.repository.CcInfoPeerRepository;
 import com.brchain.core.chaincode.repository.CcInfoRepository;
+import com.brchain.core.channel.dto.ChannelSummaryDto;
 import com.brchain.core.channel.entitiy.ChannelInfoEntity;
 import com.brchain.core.channel.entitiy.ChannelInfoPeerEntity;
 import com.brchain.core.channel.repository.ChannelInfoPeerRepository;
@@ -27,7 +26,12 @@ import com.brchain.core.container.repository.ConInfoRepository;
 @DataJpaTest(showSql = false)
 //@Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class ChaincodeDatabaseTest {
+public class ChannalDatabaseTest {
+
+	@Autowired
+	private ChannelInfoPeerRepository channelInfoPeerRepository;
+	@Autowired
+	private ChannelInfoRepository channelInfoRepository;
 
 	@Autowired
 	private CcInfoRepository ccInfoRepository;
@@ -37,11 +41,6 @@ public class ChaincodeDatabaseTest {
 	private CcInfoChannelRepository ccInfoChannelRepository;
 	@Autowired
 	private ConInfoRepository conInfoRepository;
-
-	@Autowired
-	private ChannelInfoPeerRepository channelInfoPeerRepository;
-	@Autowired
-	private ChannelInfoRepository channelInfoRepository;
 
 	@BeforeEach
 	public void setup() {
@@ -136,7 +135,7 @@ public class ChaincodeDatabaseTest {
 		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
 		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity1, ccInfoEntity3);
 		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
-		
+
 		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity2, ccInfoEntity1);
 		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
 		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity2, ccInfoEntity2);
@@ -144,119 +143,26 @@ public class ChaincodeDatabaseTest {
 
 		ccInfoChannelEntity = createCcInfoChannelEntity(channelInfoEntity2, ccInfoEntity1);
 		ccInfoChannelEntity = ccInfoChannelRepository.save(ccInfoChannelEntity);
-		
-		
 
 	}
 
+	//트렌젝션, 블록 데이터 추가 해야됨
 	@Test
-	public void 체인코드_피어_정보_조회_테스트1() throws Exception {
+	public void 채널_요약_조회_테스트() throws Exception {
 
-		System.out.println("************************ 체인코드_피어_정보_조회_테스트1 시작 ************************");
-
-		// given
-		ConInfoEntity conInfoEntity = conInfoRepository.findByConPort("1111").get();
-
-		// when
-		CcInfoPeerEntity result = ccInfoPeerRepository.findByConInfoEntity(conInfoEntity).get(0);
-
-		// then
-		System.out.println(result);
-		assertThat(conInfoEntity.getConPort()).isEqualTo("1111");
-
-		System.out.println("************************ 체인코드_피어_정보_조회_테스트1 종료 ************************");
-
-	}
-
-	// 아직 준비중
-	@Test
-	public void 체인코드_피어_정보_조회_테스트2() throws Exception {
-
-		System.out.println("************************ 체인코드_피어_정보_조회_테스트2 시작 ************************");
+		System.out.println("************************ 채널_요약_조회_테스트 시작 ************************");
 
 		// given
 
 		// when
-		List<CcInfoPeerEntity> result = ccInfoPeerRepository.findByCcId(1l);
+		List<ChannelSummaryDto> result = channelInfoRepository.findChannelSummary("202105", "202106");
 
 		// then
+
 		System.out.println(result);
-//		assertThat(conInfoEntity.getConPort()).isEqualTo("1111");
+//		assertThat(result.size()).isEqualTo(5);
 
-		System.out.println("************************ 체인코드_피어_정보_조회_테스트2 종료 ************************");
-
-	}
-
-	@Test
-	public void 활성화_가능한_체인코드_조회_테스트() throws Exception {
-
-		System.out.println("************************ 활성화_가능한_체인코드_조회_테스트 시작 ************************");
-
-		// given
-
-		// when
-		List<CcInfoPeerEntity> result = ccInfoPeerRepository.findCcInfoPeerToActive("haha-channel");
-
-		// then
-		System.out.println(result);
-		assertThat(result.size()).isEqualTo(1);
-
-		System.out.println("************************ 활성화_가능한_체인코드_조회_테스트 종료 ************************");
-
-	}
-
-	@Test
-	public void 체인코드_요약_조회_테스트() throws Exception {
-
-		System.out.println("************************ 체인코드_요약_조회_테스트 시작 ************************");
-
-		// given
-
-		// when
-		List<CcSummaryDto> result = ccInfoPeerRepository.findChaincodeSummary();
-
-		// then
-		System.out.println(result);
-		assertThat(result.size()).isEqualTo(5);
-
-		System.out.println("************************ 체인코드_요약_조회_테스트 종료 ************************");
-
-	}
-	
-	@Test
-	public void 체인코드_채널_조회_테스트1() throws Exception {
-
-		System.out.println("************************ 체인코드_채널_조회_테스트1 시작 ************************");
-
-		// given
-
-		// when
-		List<CcInfoChannelEntity> result = ccInfoChannelRepository.findByChannelName("test-channel");
-
-		// then
-		System.out.println(result);
-		assertThat(result.size()).isEqualTo(3);
-
-		System.out.println("************************ 체인코드_채널_조회_테스트1 종료 ************************");
-
-	}
-	
-	@Test
-	public void 체인코드_채널_조회_테스트2() throws Exception {
-
-		System.out.println("************************ 체인코드_채널_조회_테스트2 시작 ************************");
-
-		// given
-
-		// when
-		CcInfoChannelEntity result = ccInfoChannelRepository.findByChannelNameAndCcName("test-channel","test-chaincode").get();
-
-		// then
-		System.out.println(result);
-		assertThat(result.getChannelInfoEntity().getChannelName()).isEqualTo("test-channel");
-		assertThat(result.getCcInfoEntity().getCcName()).isEqualTo("test-chaincode");
-		
-		System.out.println("************************ 체인코드_채널_조회_테스트2 종료 ************************");
+		System.out.println("************************ 채널_요약_조회_테스트 종료 ************************");
 
 	}
 
@@ -309,10 +215,14 @@ public class ChaincodeDatabaseTest {
 
 		ChannelInfoEntity channelInfoEntity = new ChannelInfoEntity();
 
+		Random random = new Random();
+
+		int txCnt = random.nextInt(1000);
+
 		channelInfoEntity.setChannelName(channelName);
 		channelInfoEntity.setOrderingOrg("testorderer");
-		channelInfoEntity.setChannelTx(0);
-		channelInfoEntity.setChannelBlock(0);
+		channelInfoEntity.setChannelTx(txCnt);
+		channelInfoEntity.setChannelBlock(txCnt / 2);
 		channelInfoEntity.setAppAdminPolicyType("ImplicitMeta");
 		channelInfoEntity.setAppAdminPolicyValue("ANY Admins");
 		channelInfoEntity.setChannelAdminPolicyType("ImplicitMeta");
